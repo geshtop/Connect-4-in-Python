b0e1ccc606b784e3a81556c0f751d3b9bae1ce20
Geula Shoshan <g@geshtop.com>
2020-05-14 10:02:34 +0300

    change victories

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ rows = 6
 columns = 7
 
 
-class game :
+class game:
     board = []
     size = rows * columns
     playTurn = HUMAN
@@ -30,11 +30,11 @@ class game :
     '''
 
 
-def create(s) :
+def create(s):
     # Returns an empty board. The human plays first.
     # create the board
     s.board = []
-    for i in range(rows) :
+    for i in range(rows):
         s.board = s.board + [columns * [0]]
 
     s.playTurn = HUMAN
@@ -44,7 +44,7 @@ def create(s) :
     # return [board, 0.00001, playTurn, r*c]     # 0 is TIE
 
 
-def cpy(s1) :
+def cpy(s1):
     # construct a parent DataFrame instance
     s2 = game()
     s2.playTurn = s1.playTurn
@@ -54,156 +54,174 @@ def cpy(s1) :
     return s2
 
 
-def value(s) :
+def value(s):
     # Returns the heuristic value of s
-    if find_horizontal(s) or find_vertical(s) or find_pos_diagonal(s) or find_neg_diagonal(s) :
-        if s.playTurn == HUMAN : #המחשב ניצח
+    if check_single_victory(s) :
+        if s.playTurn == HUMAN:  # המחשב ניצח
             return VICTORY
-        else : #השחקן ניצח
+        else:  # השחקן ניצח
             return LOSS
-    if s.size == 0 : #תיקו
+    if s.size == 0:  # תיקו
         return TIE  # or return 0-- no better
 
     return random.random() * 10
 
 
+def check_single_victory(s):
+    if sum(find_horizontal(s)) + sum(find_vertical(s)) + sum(find_pos_diagonal(s)) +sum(find_neg_diagonal(s)) > 0:
+        return True
+    return False
+
+
 def find_horizontal(s):
+    victories = [0, 0]  # מערך, כאשר האלמנט הראשון זה הנצחונות של המחשב הערך השני זה מספר הנצחונות של האדם
     for i in range(rows):
         for j in [0, 1, 2, 3]:
-            if (s.board[i][j] + s.board[i][j+1] + s.board[i][j+2] + s.board[i][j+3] == 20):
-                return True
-            elif (s.board[i][j] + s.board[i][j+1] + s.board[i][j+2] + s.board[i][j+3] == 4):
+            if (s.board[i][j] + s.board[i][j + 1] + s.board[i][j + 2] + s.board[i][j + 3] == 20):
+                victories[0] = victories[0] + 1
+               #return True
+            elif (s.board[i][j] + s.board[i][j + 1] + s.board[i][j + 2] + s.board[i][j + 3] == 4):
+                victories[1] = victories[1] + 1
                 s.playTurn = COMPUTER
-                return True
-    return False
+                #return True
+    return victories
 
 
 def find_vertical(s):
+    victories = [0, 0]  # מערך, כאשר האלמנט הראשון זה הנצחונות של המחשב הערך השני זה מספר הנצחונות של האדם
     for i in [0, 1, 2]:
         for j in range(columns):
             if (s.board[i][j] + s.board[i + 1][j] + s.board[i + 2][j] + s.board[i + 3][j] == 20):
-                return True
+                victories[0] = victories[0] + 1
+                #return True
             elif (s.board[i][j] + s.board[i + 1][j] + s.board[i + 2][j] + s.board[i + 3][j] == 4):
+                victories[1] = victories[1] + 1
                 s.playTurn = COMPUTER
-                return True
-    return False
+                #return True
+    return victories
 
 
 def find_pos_diagonal(s):
+    victories = [0, 0]  # מערך, כאשר האלמנט הראשון זה הנצחונות של המחשב הערך השני זה מספר הנצחונות של האדם
     arrRows = [0, 1, 2]
     arrColumns = [3, 4, 5, 6]
     for i in arrRows:
         for j in arrColumns:
             if (s.board[i][j] + s.board[i + 1][j - 1] + s.board[i + 2][j - 2] + s.board[i + 3][j - 3] == 20):
-                return True
+                victories[0] = victories[0] + 1
+                #return True
             elif (s.board[i][j] + s.board[i + 1][j - 1] + s.board[i + 2][j - 2] + s.board[i + 3][j - 3] == 4):
+                victories[1] = victories[1] + 1
                 s.playTurn = COMPUTER
-                return True
-    return False
+                #return True
+    return victories
 
 
 def find_neg_diagonal(s):
+    victories = [0, 0]  # מערך, כאשר האלמנט הראשון זה הנצחונות של המחשב הערך השני זה מספר הנצחונות של האדם
     arrRows = [0, 1, 2]
     arrColumns = [0, 1, 2, 3]
     for i in arrRows:
         for j in arrColumns:
             if (s.board[i][j] + s.board[i + 1][j + 1] + s.board[i + 2][j + 2] + s.board[i + 3][j + 3] == 20):
-                return True
+                victories[0] = victories[0] + 1
+                #return True
             elif (s.board[i][j] + s.board[i + 1][j + 1] + s.board[i + 2][j + 2] + s.board[i + 3][j + 3] == 4):
+                victories[1] = victories[1] + 1
                 s.playTurn = COMPUTER
-                return True
-    return False
+                #return True
+    return victories
 
 
 def printState(s):
     # Prints the board. The empty cells are printed as numbers = the cells name(for input)
     # If the game ended prints who won.
-    for r in range(rows) :
+    for r in range(rows):
         print("\n|", end="")
         # print("\n",len(s[0][0])*" --","\n|",sep="", end="")
-        for c in range(columns) :
-            if s.board[r][c] == COMPUTER :
+        for c in range(columns):
+            if s.board[r][c] == COMPUTER:
                 print("X|", end="")
-            elif s.board[r][c] == HUMAN :
+            elif s.board[r][c] == HUMAN:
                 print("O|", end="")
-            else :
+            else:
                 print(" |", end="")
 
     print()
 
-    for i in range(columns) :
+    for i in range(columns):
         print(" ", i, sep="", end="")
 
     print()
 
     val = value(s)
 
-    if val == VICTORY :
+    if val == VICTORY:
         print("I won!")
-    elif val == LOSS :
+    elif val == LOSS:
         print("You beat me!")
-    elif val == TIE :
+    elif val == TIE:
         print("It's a TIE")
 
 
-def isFinished(s) :
+def isFinished(s):
     # Seturns True iff the game ended
     return value(s) in [LOSS, VICTORY, TIE] or s.size == 0
 
 
-def isHumTurn(s) :
+def isHumTurn(s):
     # Returns True iff it is the human's turn to play
     return s.playTurn == HUMAN
 
 
-def decideWhoIsFirst(s) :
+def decideWhoIsFirst(s):
     # The user decides who plays first
-    if int(input("Who plays first? 1-me / anything else-you : ")) == 1 :
+    if int(input("Who plays first? 1-me / anything else-you : ")) == 1:
         s.playTurn = COMPUTER
-    else :
+    else:
         s.playTurn = HUMAN
 
     return s.playTurn
 
 
-def makeMove(s, c) :
+def makeMove(s, c):
     # Puts mark (for huma. or comp.) in col. c
     # and switches turns.
     # Assumes the move is legal.
 
     r = 0
-    while r < rows and s.board[r][c] == 0 :
+    while r < rows and s.board[r][c] == 0:
         r += 1
 
     s.board[r - 1][c] = s.playTurn  # marks the board
     s.size -= 1  # one less empty cell
-    if (s.playTurn == COMPUTER) :
+    if (s.playTurn == COMPUTER):
         s.playTurn = HUMAN
-    else :
+    else:
         s.playTurn = COMPUTER
 
 
-def inputMove(s) :
+def inputMove(s):
     # Reads, enforces legality and executes the user's move.
 
     # self.printState()
     flag = True
-    while flag :
+    while flag:
         c = int(input("Enter your next move: "))
-        if c < 0 or c >= columns or s.board[0][c] != 0 :
+        if c < 0 or c >= columns or s.board[0][c] != 0:
             print("Illegal move.")
 
-        else :
+        else:
             flag = False
             makeMove(s, c)
 
 
-def getNext(s) :
+def getNext(s):
     # returns a list of the next states of s
     ns = []
-    for c in list(range(columns)) :
+    for c in list(range(columns)):
         print("c=", c)
-        if s.board[0][c] == 0 :
+        if s.board[0][c] == 0:
             print("possible move ", c)
             tmp = cpy(s)
             makeMove(tmp, c)
@@ -214,5 +232,5 @@ def getNext(s) :
     return ns
 
 
-def inputComputer(s) :
+def inputComputer(s):
     return alphaBetaPruning.go(s)
